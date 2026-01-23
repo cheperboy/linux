596b2f00bb354ca31cc44c9e0769f4c321140f7d
cheperboy <79538+cheperboy@users.noreply.github.com>
2026-01-23 14:11:47 +0100

    Update alias.sh

--- scripts/dotfiles/alias.sh
+++ scripts/dotfiles/alias.sh
@@ -1,2 +1,4 @@
-alias .. = 'cd ..'
-alias lsblk = lsblk -a -o NAME,LABEL,MOUNTPOINT,SIZE,FSUSE%,MODEL,UUID
+alias ..='cd ..'
+alias lsblk=lsblk -a -o NAME,LABEL,MOUNTPOINT,SIZE,FSUSE%,MODEL,UUID
+# xclip to copy file content to clipboard. require : sudo apt install xclip
+alias cpc="xclip -sel c < " # use it like this: cpc file_name
